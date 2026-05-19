b3187df462dfee3d2bcb524fbe9824cff97a352b
许志元 <750586056@qq.com>
2026-05-19 23:48:24 +0800

    修改三色同顺

--- src/main/java/org/mahjong4j/yaku/normals/SanshokudohjunResolver.java
+++ src/main/java/org/mahjong4j/yaku/normals/SanshokudohjunResolver.java
@@ -33,24 +33,44 @@ public class SanshokudohjunResolver extends SanshokuResolver implements NormalYa
             return false;
         }
 
-        Shuntsu candidate = null;
+//        Boolean mark_found = false;
+        Shuntsu candidate1 = null;
+        Shuntsu candidate2 = null;
+        Shuntsu real_canditate = null;
 
         for (Shuntsu shuntsu : shuntsuList) {
             TileType shuntsuType = shuntsu.getTile().getType();
             int shuntsuNum = shuntsu.getTile().getNumber();
 
-            if (candidate == null) {
-                candidate = shuntsu;
+            if (candidate1 == null) {
+                candidate1 = shuntsu;
                 continue;
             }
 
-            if (candidate.getTile().getNumber() == shuntsuNum) {
-                detectType(shuntsuType);
-                detectType(candidate.getTile().getType());
-            } else {
-//                candidate = shuntsu;
+            if(real_canditate == null){
+                if (candidate1.getTile().getNumber() == shuntsuNum) {
+                    real_canditate = candidate1;
+                    detectType(shuntsuType);
+                    detectType(candidate1.getTile().getType());
+                } else {
+                    if(candidate2 == null){
+                        candidate2 = shuntsu;
+                    }
+                    else{
+                        if(candidate2.getTile().getNumber() == shuntsuNum){
+                            real_canditate = candidate2;
+                            detectType(shuntsuType);
+                            detectType(candidate2.getTile().getType());
+                        }
+                    }
+                }
                 continue;
             }
+
+            if(real_canditate.getTile().getNumber() == shuntsuNum){
+                detectType(shuntsuType);
+            }
+
         }
         return manzu && pinzu && sohzu;
     }
